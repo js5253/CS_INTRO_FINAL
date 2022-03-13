62836d67cec349d05f53c8fa724da57a21b8e136
Gabriele A. Ron <macrocode@yahoo.com>
2022-03-13 06:22:58 -0500

    Added getRootGroups() to WUF.py

--- WUF.py
+++ WUF.py
@@ -1,11 +1,8 @@
 class WUF:
-    """This class implements the Weighted Union Find (WUF) data structure for contact tracing """
+    """This class implements the Weighted Union Find (WUF) data structure for contact tracing."""
 
-    Pair = tuple[str, int]
+    Pair = list[str, int]
     """A data type used to combine the root and size information into a single location."""
-
-    tree = {}
-    """The tree is used to store the data."""
 
     def __init__(self, iTree: dict[str, Pair] = None):
         """initializes the WUF.
@@ -20,7 +17,8 @@ class WUF:
         if not iTree:
             iTree = {}
 
-        self.tree = iTree
+        self.tree: dict[str, WUF.Pair] = iTree
+        """Used to store the WUF tree with the student ID and it's corresponding root information."""
 
     def getRoot(self, uid: str) -> str:
         """returns the root of the union the id is in
@@ -40,10 +38,33 @@ class WUF:
 
         return root
 
+    def getRootGroups(self) -> dict[str, list[str]]:
+        """returns the tree in the format of root groups
+
+        This function will parse the tree and return it as root groups such that the top level roots are the dictionary
+        keys and a list of UIDs is the value.
+
+        :param self: self
+        :returns: the root group version of the tree
+        :rtype: dict[str, list[str]]
+        """
+        rootGroups: dict[str, list[str]] = {}
+
+        for key in self.tree:
+            root: str = self.getRoot(key)
+
+            if root not in rootGroups:
+                rootGroups[root] = []
+
+            if key not in rootGroups[root]:
+                rootGroups[root].append(key)
+
+        return rootGroups
+
     def union(self, p: str, q: str) -> None:
         """unions two items in the tree
 
-        Unions data in the tree using the WUF alorithm.
+        Unions data in the tree using the WUF algorithm.
         If the size of q's root union is greater than the size of p's root union, then add p to q.
         Otherwise, q will be added to p.
 
