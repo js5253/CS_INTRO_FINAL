200b54cb38b1afa4eaa9fc1c3eb916fa89d60f7a
Gabriele A. Ron <macrocode@yahoo.com>
2022-03-14 01:19:42 -0500

    Separated the code into packages to allow for easier access, usage, and testing

---
+++ tests/testContactTracer.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestContactTracer(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
