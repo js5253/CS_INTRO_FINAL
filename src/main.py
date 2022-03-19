2793ba1161bd97fef8fde05b13adce707d8cb68b
Jose Sanchez <josersanchez0117@gmail.com>
2022-03-19 16:52:56 -0500

    WIP - Rewrote getInput to be more compact; still working to finish project

--- src/main.py
+++ src/main.py
@@ -6,56 +6,33 @@ ACCEPTABLE_DAYS = ['monday', 'tuesday', 'wednesday',
 
 database = []
 UUIDMapping = {}
-
-def getUserInput(person: str, previousContact=None) -> None:
-    '''Function appends data into global "database."'''
-
-    ob = {}
-
+def getInput(personName: str, previousContact=None):
+    days = {}
     while True:
-        locationExposed = input(
-            'Where do you remember visiting the last week? Enter one place. Or: type that\'s it. ')
-        if locationExposed == "that\'s it":
-            break
-        dayExposed = input(
-            'What day were you in this place? Type in the name of the day. ')
-        if dayExposed.lower().strip() not in ACCEPTABLE_DAYS:
-            print("You have to answer one of the days - let's try this again")
-            continue
-        if previousContact:
-            ob[previousContact[dayExposed]] = [previousContact[locationExposed], []]
-
-        othersExposed = []
+        if personName not in UUIDMapping: UUIDMapping[personName] = str(getNewUUID())
+        location = input("Where do you remember visiting last week? Make sure to only enter one place, and type that\'s it. We'll ask information about this place first. ")
+        day = input('What day were you here? (Monday, Tuesday, etc...) ')
+        if day.lower() not in ACCEPTABLE_DAYS: print("That's not a day... Let's start this over."); continue
+        peopleExposed = []
         while True:
-            e = input(
-                "Who do you remember was with or near you at this place? Type only one person at a time. Or: type that's it. ")
-            if e == "that's it":
-                ob[dayExposed] = [locationExposed, othersExposed]
-                break
-            othersExposed.append(e)
-            if e not in UUIDMapping:
-                UUIDMapping[e] = str(getNewUUID())
-            ob[dayExposed] = [locationExposed, othersExposed]
-        morePlaces = input("Okay. Have you gone more places? ")
-        if morePlaces.lower() != "yes":
-            break
-    if len(othersExposed) > 0:
-        getSecondaryContact(othersExposed)
-        pass
-    if person not in UUIDMapping:
-        UUIDMapping[person] = str(getNewUUID())
-    database.append([person, UUIDMapping[person], ob])
-    print(database)
-
+            person = input("Who else was exposed? If there's nobody else, say 'that's it'. ")
+            if person == "that's it": break
+            peopleExposed.append(person)
+            if person not in UUIDMapping: UUIDMapping[person] = str(getNewUUID());  print(UUIDMapping)
+        addMore = input("OK, have you gone more places? ")
+        if addMore == "no": break
+        days[day] = [location, peopleExposed]
+        database.append([personName, UUIDMapping[personName], days])
+        if len(peopleExposed) > 0: getSecondaryContact(peopleExposed)
 
 def getSecondaryContact(othersExposed):
     """Asks questions to secondary contacts"""
     for person in othersExposed:
         print("The next questions are for", person, ":")
-        getUserInput(person)
+        getInput(person)
 
 if __name__ == "__main__":
     UUIDMapping = getUidFromFile("students.json")
-    getUserInput(input("What is your name?"))
+    getInput(input("What is your name? "))
     dumpUUIDtoFile("students.json", UUIDMapping)
     writeToFile(FILE_NAME, database)
