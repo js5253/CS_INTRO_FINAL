13f2421bbde47f27e79c763a1015b852d360316b
Jose Sanchez <josersanchez0117@gmail.com>
2022-03-19 18:42:45 -0500

    AAAAA

--- src/main.py
+++ src/main.py
@@ -6,30 +6,47 @@ ACCEPTABLE_DAYS = ['monday', 'tuesday', 'wednesday',
 
 database = []
 UUIDMapping = {}
+
+
 def getInput(personName: str, previousContact=None):
     days = {}
+    placesBeen = []
+    peopleEncountered = []
     while True:
-        if personName not in UUIDMapping: UUIDMapping[personName] = str(getNewUUID())
-        location = input("Where do you remember visiting last week? Make sure to only enter one place, and type that\'s it. We'll ask information about this place first. ")
+        if personName not in UUIDMapping:
+            UUIDMapping[personName] = str(getNewUUID())
+        location = input(
+            "Where do you remember visiting last week? Make sure to only enter one place. We'll ask information about this place first. ")
+        placesBeen.append(location)
         day = input('What day were you here? (Monday, Tuesday, etc...) ')
-        if day.lower() not in ACCEPTABLE_DAYS: print("That's not a day... Let's start this over."); continue
-        peopleExposed = []
+        if day.lower() not in ACCEPTABLE_DAYS:
+            print("That's not a day... Let's start this over.")
+            continue
         while True:
-            person = input("Who else was exposed? If there's nobody else, say 'that's it'. ")
-            if person == "that's it": break
-            peopleExposed.append(person)
-            if person not in UUIDMapping: UUIDMapping[person] = str(getNewUUID());  print(UUIDMapping)
+            person = input(
+                "Who else was exposed? If there's nobody else, say 'that's it'. ")
+            if person == "that's it":
+                break
+            if person not in UUIDMapping:
+                UUIDMapping[person] = str(getNewUUID())
+                print(UUIDMapping)
+            peopleEncountered.append(UUIDMapping[person])
         addMore = input("OK, have you gone more places? ")
-        if addMore == "no": break
-        days[day] = [location, peopleExposed]
-        database.append([personName, UUIDMapping[personName], days])
-        if len(peopleExposed) > 0: getSecondaryContact(peopleExposed)
+        if addMore == "no":
+            days[day] = [location, peopleEncountered]
+            database.append([personName, UUIDMapping[personName], days])
+            print(database)
+            if len(peopleEncountered) > 0:
+                getSecondaryContact(peopleEncountered, [day, location])
+            return
 
-def getSecondaryContact(othersExposed):
+
+def getSecondaryContact(othersExposed, additionalInformation):
     """Asks questions to secondary contacts"""
     for person in othersExposed:
         print("The next questions are for", person, ":")
-        getInput(person)
+        getInput(person, additionalInformation)
+
 
 if __name__ == "__main__":
     UUIDMapping = getUidFromFile("students.json")
--- src/reportGenerator.py
+++ src/reportGenerator.py
@@ -3,10 +3,11 @@ import json
 from src.contactTracer import ContactTracer
 
 def returnSimplifiedForm(complexData):
-    print(complexData[2])
+    # print(complexDa/ta)
     d = {}
     for day in complexData[2]:
         d[day] = complexData[2][day][1]
+        print(d)
         
     return {
         "id": complexData[1],
@@ -21,15 +22,18 @@ def main():
         fileContents = file.read()
         tracingObj = json.loads(fileContents)
         tracer = ContactTracer()
-
-        for item in tracingObj[0]:
-            i = returnSimplifiedForm(item)
-            print(i)
-            tracer.addPerson(i['id'], i['days'])
+        for i in tracingObj:
+            e = returnSimplifiedForm(i)
+            print(e["days"])
+            print(type(e['days']))
+            tracer.addPerson(e["id"], e["days"])
+        # print(tracingObj)
+            # print(i)
+        #    tracer.addPerson(i['id'], i['days'])
         tracer.trace()
 
         for id, content in tracer.union.getRootGroups().items():
-            # assume will always return the user by itself
+            # assume will always return the user, may also return students who they spread covid to
             if len(content) > 1:
                 print(content)
                 print(f'Student {id} may have spread COVID-19 to students {", ".join(content)}')
