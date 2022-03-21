cee8ac65c223b8ed05cf2778e2358bf73eb6d262
Gabriele A. Ron <macrocode@yahoo.com>
2022-03-20 20:34:00 -0500

    Finished Project

--- src/dataGenerator.py
+++ src/dataGenerator.py
@@ -20,6 +20,8 @@ def getInput(personName: str, previousContact=None):
         if personName not in UUIDMapping:
             UUIDMapping[personName] = str(getNewUUID())
         location = input(locationString)
+        if location.lower() == "that's it":
+            return
         placesBeen.append(location)
         day = input('What day were you here? (Monday, Tuesday, etc...) ')
         if day.lower() not in ACCEPTABLE_DAYS:
@@ -28,14 +30,14 @@ def getInput(personName: str, previousContact=None):
         while True:
             person = input(
                 "Who else was exposed? If there's nobody else, say 'that's it'. ")
-            if person == "that's it":
+            if person.lower() == "that's it":
                 break
             if person not in UUIDMapping:
                 UUIDMapping[person] = str(getNewUUID())
-                print(UUIDMapping)
+                # print(UUIDMapping)
             peopleEncountered.append(UUIDMapping[person])
         addMore = input("OK, have you gone more places? ")
-        if addMore == "no":
+        if addMore.lower() == "no":
             days[day] = [peopleEncountered, placesBeen]
             database.append([personName, UUIDMapping[personName], days])
             print(database)
@@ -46,13 +48,18 @@ def getInput(personName: str, previousContact=None):
 
 def getSecondaryContact(othersExposed, additionalInformation):
     """Asks questions to secondary contacts"""
-    for id in othersExposed:
-        print("The next questions are for", searchByValue(UUIDMapping, id), ":")
-        getInput(searchByValue(UUIDMapping, id), additionalInformation)
+    for uid in othersExposed:
+        print("The next questions are for", searchByValue(UUIDMapping, uid), ":")
+        getInput(searchByValue(UUIDMapping, uid), additionalInformation)
 
 
-if __name__ == "__main__":
+def main():
+    global UUIDMapping
     UUIDMapping = getUidFromFile("students.json")
     getInput(input("What is your name? "))
     dumpUUIDtoFile("students.json", UUIDMapping)
     writeToFile(FILE_NAME, database)
+
+
+if __name__ == "__main__":
+    main()
--- src/reportGenerator.py
+++ src/reportGenerator.py
@@ -5,29 +5,37 @@ from src.contactTracer import ContactTracer
 
 UID_MAPPING = {}
 FILE_NAME = "tracing.json"
+
+
 def main():
     """Generates a report """
     print(f'Contact Report for {date.today()} Using report data from {FILE_NAME}:')
+    tracer = ContactTracer()
     with open(FILE_NAME, 'r') as file:
         fileContents = file.read()
         tracingObj = json.loads(fileContents)
-        tracer = ContactTracer()
         for i in tracingObj:
-            print(i)
+            # print(i)
             # print(e['days'])
             tracer.addPerson(i[1], i[2])
         tracer.trace()
 
-        for id, content in tracer.union.getRootGroups().items():
-            # assume will always return the user, may also return students who they spread covid to
-            content.pop(0)
-            if len(content) >= 1:
-                print(f'Student {searchByValue(UID_MAPPING, id)} had contact with students {", ".join(list(map(lambda x: searchByValue(UID_MAPPING, x).capitalize(), content)))} and could have spread COVID to them.')
-            else:
-                print(f'Student {id} did not have additional contact with students.')
+    print("\n\tDirect Contact:")
+    for name, data in tracer.people.items():
+        peeps = [searchByValue(UID_MAPPING, i[0][0]) for i in list(data.values())]
+        print(f"\t\t{searchByValue(UID_MAPPING, name)}: {', '.join(peeps)}")
 
-        print("Report End")
-    pass
+    print("\n\tIndirect Contact Groups:")
+    for name, data in tracer.union.getRootGroups().items():
+        peeps = [searchByValue(UID_MAPPING, i) for i in data]
+        print(f"\t\t{', '.join(peeps)}")
+
+    print("\n\tPlaces & Who Visited Them:")
+    for name, data in tracer.places.items():
+        peeps = [searchByValue(UID_MAPPING, i[0]) for i in list(data.values())]
+        print(f"\t\t{name}: {', '.join(peeps)}")
+
+    print("\nReport End")
 
 
 if __name__ == "__main__":
